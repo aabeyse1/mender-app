db5b55c58c44578ffbe1e37110e041d5f63037bd
JustinChalichemala <jchalich@macalester.edu>
2023-11-19 21:46:42 -0600

    added image and header to match page

--- mender/src/pages/MatchApp.js
+++ mender/src/pages/MatchApp.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MatchApp.css'; // Make sure to create a CSS file for your styles
+import placeholderImage from './placeholder.png'
 
 const MatchPage = () => {
   const handleReject = () => {
@@ -16,6 +17,15 @@ const MatchPage = () => {
 
   return (
     <div>
+
+    <div class="topNav">
+          <p class="navTitle">Mender</p>
+          <div class="navProfile">
+              <img src={placeholderImage} class="profileImg" alt="Profile" />
+              <h1 class="profileText">First Last Name</h1>
+          </div>
+      </div>
+
       <ul>
         <li><a href="default.asp">Home</a></li>
         <li><a href="news.asp">Matches</a></li>
@@ -26,6 +36,9 @@ const MatchPage = () => {
         <button id="rejectButton" className="btn btn-danger btn-lg" onClick={handleReject}>
           Reject
         </button>
+
+        <img src="https://assets-global.website-files.com/5e66ca0250be6103da645a88/64370e06159764728a6c6554_Account%20Executive.webp" alt="Profile" />
+
         <button id="acceptButton" className="btn btn-success btn-lg" onClick={handleAccept}>
           Accept
         </button>
