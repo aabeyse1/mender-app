99bc950afd1c45773f530d42588d8e550003f981
aabeyse1 <93781833+aabeyse1@users.noreply.github.com>
2023-12-17 16:29:53 -0600

    added way to create fake data

--- mender-django/djangoProject/djangoProject/fakedata.py
+++ mender-django/djangoProject/djangoProject/fakedata.py
@@ -1,10 +1,51 @@
 from mongo_connector import Mentee, Mentor, Match, Database
 
 ## testing methods with fake data
-mentor1 = Mentor.create_mentor('John Doe', 'MN', 'Tech', 'Software', 'ABC Corp', 'Linkedin')
-mentee1 = Mentee.create_mentee('Jane Smith', 'MN', 'Tech', 'Software', 'Macalester', 'Linkedin')
+# mentor1 = Mentor.create_mentor('John Doe', 'MN', 'Minneapolis', 'Tech', 'Software', 'ABC Corp', 'Linkedin')
+# mentee1 = Mentee.create_mentee('Jane Smith', 'MN', 'Minneapolis', 'Tech', 'Software', 'Macalester', 'Linkedin')
 
-Match.create_match(mentor1, mentee1)
+# Match.create_match(mentor1, mentee1)
 
 ## testing finding user
-user_id, user, user_type = Database.find_user(name='John Doe', linkedin_username='Linkedin')
+# user_id, user, user_type = Database.find_user(name='John Doe', linkedin_username='Linkedin')
+# if user:
+#     print(f"{user_type.capitalize()} found with ID {user_id}: {user}")
+# else:
+#     print("User not found")
+
+
+from faker import Faker
+fake = Faker()
+
+# Function to generate fake mentor data
+def generate_fake_mentor():
+    return {
+        'name': fake.name(),
+        'area': fake.state(),
+        'hometown': fake.city(),
+        'industry': fake.job(),
+        'interests': fake.job(),
+        'company': fake.company(),
+        'contact': fake.user_name(),
+    }
+
+def generate_fake_mentee():
+    return {
+        'name': fake.name(),
+        'area': fake.state(),
+        'hometown': fake.city(),
+        'industry': fake.job(),
+        'interests': fake.job(),
+        'college': fake.company(),
+        'contact': fake.user_name(),
+    }
+
+# Populate mentors collection with fake data
+for _ in range(2):  # Generate 10 fake mentors
+    mentor_data = generate_fake_mentor()
+    mentor_id = Mentor.create_mentor(**mentor_data)
+
+# Populate mentees collection with fake data
+for _ in range(2):  # Generate 10 fake mentees
+    mentee_data = generate_fake_mentee()
+    mentee_id = Mentee.create_mentee(**mentee_data)
--- mender-django/djangoProject/djangoProject/mongo_connector.py
+++ mender-django/djangoProject/djangoProject/mongo_connector.py
@@ -1,71 +1,5 @@
 from pymongo import MongoClient
 
-class Mentor:
-    def __init__(self, name, area, interests, industry, company, contact, matches=None):
-        self.name = name
-        self.company = company
-        self.area = area
-        self.interests = interests
-        self.industry = industry
-        self.contact = contact or []
-        self.matches = matches or []
-
-    def create_mentor(name, area, interests, industry, company, contact):
-        mentor = Mentor(name, area, interests, industry, company, contact, matches=None)
-        mentor_id = mentors_collection.insert_one(vars(mentor)).inserted_id
-        return mentor_id
-
-class Mentee:
-    def __init__(self, name, area, industry, interests, college, contact, matches=None):
-        self.name = name
-        self.area = area
-        self.industry = industry
-        self.interests = interests
-        self.college = college
-        self.contact = contact or []
-        self.matches = matches or []
-    
-    def create_mentee(name, area, industry, interests, college, contact):
-        mentee = Mentee(name, area, interests, industry, college, contact, matches=None)
-        mentee_id = mentee_collection.insert_one(vars(mentee)).inserted_id
-        return mentee_id
-
-class Match:
-    def __init__(self, mentor_id, mentee_id):
-        self.mentor_id = mentor_id
-        self.mentee_id = mentee_id  
-
-    def create_match(mentor_id, mentee_id):
-        match = Match(mentor_id=mentor_id, mentee_id=mentee_id)
-        match_id = matches_collection.insert_one(vars(match)).inserted_id
-
-        # Update the mentor's matches list
-        mentor = mentors_collection.find_one({'_id': mentor_id})
-        mentor_matches = mentor.get('matches', [])
-        mentor_matches.append(match_id)
-        mentors_collection.update_one({'_id': mentor_id}, {'$set': {'matches': mentor_matches}})
-
-        # Update the mentee's matches list
-        mentee = mentee_collection.find_one({'_id': mentee_id})
-        mentee_matches = mentee.get('matches', [])
-        mentee_matches.append(match_id)
-        mentee_collection.update_one({'_id': mentee_id}, {'$set': {'matches': mentee_matches}})
-
-        return match_id    
-
-class Database:
-    def find_user(name, linkedin_username):
-        mentor = mentors_collection.find_one({'name': name, 'contact': linkedin_username})
-        mentee = mentee_collection.find_one({'name': name, 'contact': linkedin_username})
-
-        if mentor:
-            return mentor['_id'], mentor, 'mentor'
-        elif mentee:
-            return mentee['_id'], mentee, 'mentee'
-        else:
-            return None, None, None
- 
-    
 connection_string = "mongodb+srv://cluster0.yl9k9ng.mongodb.net/"
 username = "mender-admin"
 password = "446projxyz"   
@@ -75,8 +9,96 @@ database_name = "mender"
 client = MongoClient(connection_string, username=username, password=password)
 mongo_db = client[database_name]
 
-# get collections
-mentors_collection = mongo_db['mentor']
-mentee_collection = mongo_db['mentee']
-credentials_collection = mongo_db['credential']
-matches_collection = mongo_db['match']
+class Mentor:
+    def __init__(self, name, area, hometown, interests, industry, company, contact, matches=None):
+        self.name = name
+        self.company = company
+        self.area = area
+        self.hometown = hometown
+        self.interests = interests
+        self.industry = industry
+        self.contact = contact or []
+        self.matches = matches or []
+
+    @staticmethod
+    def get_mentor_collection():
+        mentor_collection = mongo_db['mentor']
+        return mentor_collection
+    
+    @staticmethod
+    def create_mentor(name, area, hometown, interests, industry, company, contact):
+        mentor = Mentor(name, area, hometown, interests, industry, company, contact, matches=None)
+        mentor_id = Mentor.get_mentor_collection().insert_one(vars(mentor)).inserted_id
+        return mentor_id
+    
+    
+
+class Mentee:
+    def __init__(self, name, area, hometown, industry, interests, college, contact, matches=None):
+        self.name = name
+        self.area = area
+        self.hometown = hometown
+        self.industry = industry
+        self.interests = interests
+        self.college = college
+        self.contact = contact or []
+        self.matches = matches or []
+
+    @staticmethod
+    def get_mentee_collection():
+        mentee_collection = mongo_db['mentee']
+        return mentee_collection
+    
+    @staticmethod
+    def create_mentee(name, area, hometown, industry, interests, college, contact):
+        mentee = Mentee(name, area, hometown, interests, industry, college, contact, matches=None)
+        mentee_id = Mentee.get_mentee_collection().insert_one(vars(mentee)).inserted_id
+        return mentee_id
+
+class Match:
+    def __init__(self, mentor_id, mentee_id):
+        self.mentor_id = mentor_id
+        self.mentee_id = mentee_id  
+
+    @staticmethod
+    def get_matches_collection():
+        match_collection = mongo_db['match']
+        return match_collection
+
+    @staticmethod
+    def create_match(mentor_id, mentee_id):
+        match = Match(mentor_id=mentor_id, mentee_id=mentee_id)
+        match_id = Match.get_matches_collection().insert_one(vars(match)).inserted_id
+
+        # Update the mentor's matches list
+        mentor = Mentor.get_mentor_collection().find_one({'_id': mentor_id})
+        mentor_matches = mentor.get('matches', [])
+        mentor_matches.append(match_id)
+        Mentor.get_mentor_collection().update_one({'_id': mentor_id}, {'$set': {'matches': mentor_matches}})
+
+        # Update the mentee's matches list
+        mentee = Mentee.get_mentee_collection().find_one({'_id': mentee_id})
+        mentee_matches = mentee.get('matches', [])
+        mentee_matches.append(match_id)
+        Mentee.get_mentee_collection().update_one({'_id': mentee_id}, {'$set': {'matches': mentee_matches}})
+
+        return match_id    
+
+class Database:
+
+    @staticmethod
+    def get_credentials_collection():
+        credentials_collection = mongo_db['credential']
+        return credentials_collection
+    
+    @staticmethod
+    def find_user(name, linkedin_username):
+        mentor = Mentor.get_mentor_collection().find_one({'name': name, 'contact': linkedin_username})
+        mentee = Mentee.get_mentee_collection().find_one({'name': name, 'contact': linkedin_username})
+
+        if mentor:
+            return mentor['_id'], mentor, 'mentor'
+        elif mentee:
+            return mentee['_id'], mentee, 'mentee'
+        else:
+            return None, None, None
